8a9091b3a95f675c1645a7eebe281d968d600288
Import-Saurabh <saurabhhadole50@gmail.com>
2026-05-31 16:03:38 +0530

    remove hardcored credentials

--- database/db_mysql.py
+++ database/db_mysql.py
@@ -7,7 +7,7 @@ import pymysql
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Avinash18")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME = os.getenv("DB_NAME", "ai_hedge_fund")
 DB_PORT = int(os.getenv("DB_PORT", "3306"))
 
